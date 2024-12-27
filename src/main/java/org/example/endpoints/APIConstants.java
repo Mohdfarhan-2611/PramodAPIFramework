400a1a5b760826faa9666d0c0b520ea18c0e1b19
mhdfa <mhdfarhan2611@gmail.com>
2024-12-27 10:02:29 +0530

    Initial commit

--- src/main/java/org/example/endpoints/APIConstants.java
+++ src/main/java/org/example/endpoints/APIConstants.java
@@ -1,6 +1,12 @@
 package org.example.endpoints;
 
+import java.net.URL;
+
 public class APIConstants {
 
+    public static String BASE_URL = "https://restful-booker.herokupp.com";
+    public static String CREATE_UPDATE_BOOKING_URL = "/booking";
+    public static String AUTH_URL = "/auth";
+    public static String PING_URL = "/ping";
 
 }
